2c9b81852d464d94c3aacf5beb9c7fcd4e2a7a1b
june jepkirui <jepkiruijune@gmail.com>
2024-07-02 20:37:51 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!";
+  }  
+  else if (feet <= 2000) {  
+    return "That will be twenty bucks.";}
+  else if (feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
+  }
+
+  // Write your code here!
+
+
+function ternaryCheckCity(checkCity){
+  if (checkCity==='NYC'){
+  return "Ok, sounds good."
+}
+else if(checkCity!='NYC'){
+  return "No go."
+
+}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
   // Write your code here!
 }
